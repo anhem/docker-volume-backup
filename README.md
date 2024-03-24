7cfcad1a6825d7515fb9d1a895e637bd0e798723
Andreas Hemmingsson <andhem@gmail.com>
2024-03-24 12:12:10 +0100

    updated script

--- README.md
+++ README.md
@@ -26,6 +26,23 @@ docker run --rm -v /data/backup:/backup -v hello_world_volume:/etc/hello/world b
 
 ## Automatic backup
 
-## Cron
+### Cron
 
 Schedule as a cron job to run every night at 01:00 with `crontab -e` and add `0 1 * * * /path/to/dock_volume_backup.sh <backup directory>`
+
+## NFS
+
+This will mount an NFS share before running the backup script and then unmount it when done.
+
+`mount_nfs_and_backup.sh <server> <share> <mount point> [<skip containers>]`
+
+to use an NFS share create a mount point and mark it as immutable to prevent anyone from writing to it
+
+Example:
+```
+mkdir -p /mnt/docker/
+chattr +i /mnt/docker/
+mount_nfs_and_backup.sh 192.168.1.2 /path/on/server/ /mnt/docker/ mariadb mongodb 
+```
+
+
--- mount_nfs_and_backup.sh
+++ mount_nfs_and_backup.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-mount_nfs_share() {
-    local nfs_server="$1"
-    local nfs_share="$2"
-    local mount_point="$3"
-
-    echo "Mounting NFS share..."
-    if mount -t nfs -o rw,noatime,rsize=8192,wsize=8192,tcp,timeo=14,nfsvers=4 "$nfs_server:$nfs_share" "$mount_point"; then
-        echo "NFS share mounted successfully."
-        return 0
-    else
-        echo "Failed to mount NFS share."
-        return 1
-    fi
-}
-
-execute_backup_script() {
-    local backup_script="$1"
-    local mount_point="$2"
-    shift 2
-    local backup_script_args=("$@")
-
-    echo "Calling $backup_script..."
-    if [ -x "$backup_script" ]; then
-        "$backup_script" "$mount_point" "${backup_script_args[@]}"
-    else
-        echo "Error: $backup_script is not executable or does not exist."
-        exit 1
-    fi
-}
-
 unmount_nfs_share() {
     local mount_point="$1"
 
@@ -41,17 +11,35 @@ unmount_nfs_share() {
     fi
 }
 
-mount_and_backup() {
+call_backup_script() {
+    local backup_script="$1"
+    local mount_point="$2"
+    local backup_script_args=("${@:3}")
+
+    echo "Calling $backup_script..."
+    if [ -x "$backup_script" ]; then
+        "$backup_script" "$mount_point" "${backup_script_args[@]}"
+    else
+        echo "Error: $backup_script is not executable or does not exist."
+        unmount_nfs_share "$mount_point"
+        exit 1
+    fi
+}
+
+mount_nfs_share() {
     local nfs_server="$1"
     local nfs_share="$2"
     local mount_point="$3"
     local backup_script="$4"
-    shift 4
-    local backup_script_args=("$@")
+    local backup_script_args=("${@:5}")
 
-    if mount_nfs_share "$nfs_server" "$nfs_share" "$mount_point"; then
-        execute_backup_script "$backup_script" "$mount_point" "${backup_script_args[@]}"
+    echo "Mounting NFS share..."
+    if mount -t nfs -o rw,noatime,rsize=8192,wsize=8192,tcp,timeo=14,nfsvers=4 "$nfs_server:$nfs_share" "$mount_point"; then
+        echo "NFS share mounted successfully."
+        call_backup_script "$backup_script" "$mount_point" "${backup_script_args[@]}"
         unmount_nfs_share "$mount_point"
+    else
+        echo "Failed to mount NFS share."
     fi
 }
 
@@ -60,4 +48,4 @@ if [ $# -lt 3 ]; then
     exit 1
 fi
 
-mount_and_backup "$@"
+mount_nfs_share "$1" "$2" "$3" "$(dirname "${BASH_SOURCE[0]}")/docker_volume_backup.sh" "${@:4}"
