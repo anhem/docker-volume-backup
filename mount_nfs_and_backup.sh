c5266174c02617a7f435648b35a648893892b273
Andreas Hemmingsson <andhem@gmail.com>
2024-03-24 11:40:37 +0100

    updated script

--- mount_nfs_and_backup.sh
+++ mount_nfs_and_backup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <server> <share> <mount point>"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <server> <share> <mount point> [<skip containers>]"
     exit 1
 fi
 
 NFS_SERVER="$1"
 NFS_SHARE="$2"
 MOUNT_POINT="$3"
+shift 3
+BACKUP_SCRIPT_ARGS=("$@")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BACKUP_SCRIPT="$SCRIPT_DIR/docker_volume_backup.sh"
 
 echo "Mounting NFS share..."
-if mount -t nfs "$NFS_SERVER:$NFS_SHARE" "$MOUNT_POINT"; then
+if mount -t nfs -o rw,noatime,rsize=8192,wsize=8192,tcp,timeo=14,nfsvers=4 "$NFS_SERVER:$NFS_SHARE" "$MOUNT_POINT"; then
     echo "NFS share mounted successfully."
 
     echo "Calling $BACKUP_SCRIPT ..."
     if [ -x "$BACKUP_SCRIPT" ]; then
-        "$BACKUP_SCRIPT"
+        "$BACKUP_SCRIPT" "$MOUNT_POINT" "${BACKUP_SCRIPT_ARGS[@]}"
     else
         echo "Error: $BACKUP_SCRIPT is not executable or does not exist."
         exit 1
